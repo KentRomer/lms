ab922c940f891a0f59f8339015e3b1e205b1be97
Sannichigetsu <sannichigetsu@gmail.com>
2025-12-14 23:31:03 +0800

    dsds

--- routes/index.php
+++ routes/index.php
@@ -1,6 +1,8 @@
 <?php
+// At the very top of your file
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
-ini_set('error_log', '/tmp/php_errors.log');
-?>
+
+// For Railway, log to stderr so it appears in railway logs
+ini_set('error_log', 'php://stderr');
